9563d65bad9199716975ca788fe0adc5898703ab
wilsjw <wilsjw2@student.op.ac.nz>
2024-08-07 15:55:15 +1200

    feat: Created getUser

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -19,6 +19,31 @@ const getUsers = async (req, res) => {
   }
 };
 
+const getUser = async (req, res) => {
+  try {
+    const contentType = req.headers["content-type"];
+    if (!contentType || contentType !== "application/json")
+      return res.status(400).json({ "msg": "Invalid Content-Type. Expected 'application/json'." });
+
+    const user = await prisma.user.findUnique({
+      "where": { "id": String(req.params.id) },
+      "select": {
+        "id": true,
+        "name": true,
+        "email": true,
+        "skill": true,
+      },
+    });
+
+    if (!user) return res.status(404).json({ "msg": "No user found." });
+
+    return res.status(200).json({ "data": user });
+  } catch (error) {
+    return res.status(500).json({ "msg": error.message });
+  }
+};
+
 export {
   getUsers,
+  getUser,
 };
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import { getUsers, getUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUser);
 
 export default router;
