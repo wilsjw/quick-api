ce5e223454e7b9e4d3ec196e4547c2abd7f00d09
wilsjw <wilsjw2@student.op.ac.nz>
2024-08-07 15:27:08 +1200

    feat: Added scripts and route

--- back/app.js
+++ back/app.js
@@ -7,12 +7,12 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.get((req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000.");
+  console.log("Server is listening on port 3000.");
 });
 
 export default app;
