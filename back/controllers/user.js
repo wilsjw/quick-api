6d60533142e774d80a2d85721c7a9215727fc8bc
wilsjw <wilsjw2@student.op.ac.nz>
2024-08-08 10:53:34 +1200

    style: Formatted files with prettier

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -5,41 +5,39 @@ const prisma = new PrismaClient();
 const getUsers = async (req, res) => {
   try {
     const users = await prisma.user.findMany({
-      "select": {
-        "id": true,
-        "name": true,
+      select: {
+        id: true,
+        name: true,
       },
     });
 
-    if (users.length === 0) return res.status(404).json({ "msg": "No users found." });
+    if (users.length === 0)
+      return res.status(404).json({ msg: "No users found." });
 
-    return res.status(200).json({ "data": users });
+    return res.status(200).json({ data: users });
   } catch (error) {
-    return res.status(500).json({ "msg": error.message });
+    return res.status(500).json({ msg: error.message });
   }
 };
 
 const getUser = async (req, res) => {
   try {
     const user = await prisma.user.findUnique({
-      "where": { "id": String(req.params.id) },
-      "select": {
-        "id": true,
-        "name": true,
-        "email": true,
-        "skill": true,
+      where: { id: String(req.params.id) },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        skill: true,
       },
     });
 
-    if (!user) return res.status(404).json({ "msg": "No user found." });
+    if (!user) return res.status(404).json({ msg: "No user found." });
 
-    return res.status(200).json({ "data": user });
+    return res.status(200).json({ data: user });
   } catch (error) {
-    return res.status(500).json({ "msg": error.message });
+    return res.status(500).json({ msg: error.message });
   }
 };
 
-export {
-  getUsers,
-  getUser,
-};
+export { getUsers, getUser };
